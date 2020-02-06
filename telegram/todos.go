e0c94c69fa797e2c2a029c565c911e72a07fe96f
syentix <tim.gerfried@gmail.com>
2020-02-06 19:50:32 +0100

    Still nothing new

--- telegram/todos.go
+++ telegram/todos.go
@@ -10,11 +10,18 @@ import (
 	"github.com/aodin/date"
 )
 
-type toDo struct {
+type toDoJSON struct {
 	id        int    `json:"id"`
 	todo      string `json:"todo"`
 	date      string `json:"date"`
 	completed bool   `json:"completed"`
+}
+
+type toDo struct {
+	id        int
+	todo      string
+	date      string
+	completed bool
 }
 
 func AddToDo(data Package) {
@@ -37,14 +44,16 @@ func AddToDo(data Package) {
 	// Get the current JSON Data
 	jsonData, _ := ioutil.ReadAll(jsonFile)
 
-	var curJSON []toDo
+	var curJSON []toDoJSON
 	err = json.Unmarshal(jsonData, &curJSON)
+
+	newJSON := convertJsonStructToStruct(curJSON)
 
 	// Create new Struct and fill with data.
 	curDate := date.Today().String()
 	var thisID int = 0
-	if len(curJSON) != 0 {
-		thisID = getID(curJSON)
+	if len(newJSON) != 0 {
+		thisID = getID(newJSON)
 	}
 
 	var newToDo toDo
@@ -53,12 +62,10 @@ func AddToDo(data Package) {
 	newToDo.todo = data.text
 	newToDo.date = curDate
 
-	curJSON = append(curJSON, newToDo)
+	newJSON = append(newJSON, newToDo)
 
-	result, _ := json.MarshalIndent(&curJSON, "", " ")
-	s := string(result[:])
-	fmt.Println(s)
-	_ = ioutil.WriteFile(path, result, 0644)
+	result, _ := json.MarshalIndent(&newJSON, "", " ")
+	_ = ioutil.WriteFile(path, result, 0666)
 
 }
 
@@ -70,4 +77,12 @@ func getID(todos []toDo) int {
 		}
 	}
 	return c
+}
+
+func convertJsonStructToStruct(jsons []toDoJSON) []toDo {
+	var res []toDo
+	for _, json := range jsons {
+		res = append(res, toDo{json.id, json.todo, json.date, json.completed})
+	}
+	return res
 }
